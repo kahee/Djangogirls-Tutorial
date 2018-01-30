40585155eb53f18ddc25ae9b4dd181e89cff15ec
kahee <yuygh131@gmail.com>
2018-01-30 12:03:45 +0900

    templates/blog/post_list.html파일 작성, blog.views.post_list에서 해당 파일 resarch 결과 리턴

--- django/blog/views.py
+++ django/blog/views.py
@@ -12,7 +12,12 @@ def post_list(request):
     # config.urls 모듈은 ''(/admin/를 제외한 모든 요청)을 blog.urls 모듈로 전달
     # blog.urls모듈은 받은 요청의 URL과 일치하는 패턴이 있는지 검사
     # 있다면 일치하는 패턴과 연결된 함수 (view)를 실행
+    #   7.1 settings 모듈의 TEMPLATES속성 내의 DIRS목록에서 blog/post_list.html 파일의 내용을 가져옴
+    #   7.2 가져온 내용을 적절히 처리(렌더링, render()함수))하여 리턴
     # 함수의 실행 결과를 브러우저로 다시 전달
 
     # HTTP 프로토콜로 텍스트 데이터 응답
-    return HttpResponse('<html><body><h1>post list</h1><p>post목록을 보여줄 예정입니다. </p></body></html>')
+    # return HttpResponse('<html>~')
+
+    # 'blog/post_list.html'템플릿 파일을 이용해 http 프로토콜로 응답
+   return render(request, 'blog/post_list.html')
